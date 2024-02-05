204f608cbbff93e5fd77adc661d4a6b226f0afee
Pritanshu69 <samantaprit1924@gmail.com>
2024-02-05 11:51:48 +0530

    one code made without function

--- problem4b.py
+++ problem4b.py
@@ -1,19 +1,10 @@
-def remove_duplicate(input_str):
-    unique_chars = set()
-    result = []
-    
-    for char in input_str:
-        if char not in unique_chars:
-            unique_chars.add(char)
-            result.append(char)
-            
-    return''.join(result)
+input_string = input("Enter a string: ")
+result_string = ""
 
-try:
-    input_string = input("Enter a string: ")
-    
-    output_string = remove_duplicate(input_string)
-    
-    print(f"Output: {output_string}")
-except ValueError:
-    print("Error")
+for char in input_string:
+    if char not in result_string:
+        result_string += char
+
+print("String after removing duplicates:", result_string)
+
+
