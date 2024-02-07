a830981c36de0231d5fc083e5e22e468af8a5412
Pritanshu69 <samantaprit1924@gmail.com>
2024-02-07 13:00:18 +0530

    spell check

--- problem22a.py
+++ problem22a.py
@@ -3,7 +3,7 @@ age = int(input("Enter the age: "))
 if age<2:
     print("in born")
 elif age>=2 and age<=10:
-    print("chile")
+    print("child")
 elif age>=11 and age<=17:
     print("young")
 elif age>=18 and age<=49:
